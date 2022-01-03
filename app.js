3d8c32827f5cea54b86a5eafd4272690930e3cd8
Marcus Liu <me@mail.lmarcus.onl>
2022-01-03 16:04:52 +0800

    設定 handlebars 引擎；新增路由；監聽 3000 port

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const port = 3000
 // import and setting handlebars
 const exphbs = require('express-handlebars')
 app.set('view engine', 'handlebars')
-
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 // import local static files
 app.use(express.static('public'))
 
 
 // route setting
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`)
+})
