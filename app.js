b0bdb31d4b6ceccd8b1ccc372b02d2eac0f640b8
Marcus Liu <me@mail.lmarcus.onl>
2022-01-03 18:19:16 +0800

    建立搜尋餐廳的路由

--- app.js
+++ app.js
@@ -14,10 +14,15 @@ app.use(express.static('public'))
 const restaurantList = require('./restaurant.json')
 
 // route setting
+// 首頁
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
-
+// 餐廳資料
+app.get('/restaurants/:restaurantId', (req, res) => {
+  res.render('restaurantinfo')
+})
+// 搜尋
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
 })
