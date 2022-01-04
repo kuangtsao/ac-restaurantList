829137374019c1506cc474eff82691ff64307565
Marcus Liu <me@mail.lmarcus.onl>
2022-01-04 15:24:22 +0800

    完成警告功能與邏輯

--- app.js
+++ app.js
@@ -13,11 +13,15 @@ app.use(express.static('public'))
 // 餐廳清單
 const restaurantList = require('./restaurant.json')
 
+// 管理搜尋是否找到餐廳的 flag，一開始先當作有找到
+let findingStatus = true
+
 // route setting
 // 首頁
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
+  res.render('index', { restaurants: restaurantList.results, findingStatus: findingStatus})
 })
+
 // 餐廳資料
 app.get('/restaurants/:restaurantId', (req, res) => {
   const restaurant = restaurantList.results.find(function findRestaurantName(restaurant) {
@@ -26,6 +30,7 @@ app.get('/restaurants/:restaurantId', (req, res) => {
   res.render('restaurantinfo', { restaurant: restaurant })
   
 })
+
 // 搜尋
 app.get('/search', (req, res) => {
   // 搜尋字串去除空白
@@ -33,10 +38,13 @@ app.get('/search', (req, res) => {
   // 只要關鍵字符合其中一個，就返回內容到陣列
   const information = restaurantList.results.filter(information => information.category.includes(keyword) || information.name.includes(keyword) || information.name_en.toLowerCase().includes(keyword.toLowerCase()))
 
-  if (information.length === 0) {
-    res.render('index', { restaurants: restaurantList.results })
+  // 搜尋時會重複多次 /search 路由，所以每次進入都要重新管理
+  if (information.length > 0) {
+    findingStatus = true
+    res.render('index', { restaurants: information , findingStatus: findingStatus })
   } else {
-    res.render('index', { restaurants: information })
+    findingStatus = false
+    res.render('index', { restaurants: restaurantList.results, findingStatus: findingStatus })
   }
 })
 app.listen(port, () => {
