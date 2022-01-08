0844ad65bba475051bdae71fa745d6d67d42a4f0
Marcus Liu <me@mail.lmarcus.onl>
2022-01-08 17:40:06 +0800

    依據助教與同學的回饋做修改

--- app.js
+++ app.js
@@ -13,13 +13,10 @@ app.use(express.static('public'))
 // 餐廳清單
 const restaurantList = require('./restaurant.json')
 
-// 管理搜尋是否找到餐廳的 flag，一開始先當作有找到，提供給 index.handlebars 去作警告渲染
-let findingStatus = true
-
 // route setting
 // 首頁
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results, findingStatus: findingStatus })
+  res.render('index', { restaurants: restaurantList.results, findingStatus: true })
 })
 
 // 餐廳資料
@@ -32,18 +29,18 @@ app.get('/restaurants/:restaurantId', (req, res) => {
 
 // 搜尋
 app.get('/search', (req, res) => {
-  // 搜尋字串去除空白
-  const keyword = req.query.keyword.split(' ').join('')
+  // 保留原始的搜尋字串
+  const originKeyword = req.query.keyword
+  // 搜尋字串去除空白與所有關鍵字小寫
+  const keyword = originKeyword.split(' ').join('').toLowerCase()
   // 只要關鍵字符合其中一個，就返回內容到陣列
-  const information = restaurantList.results.filter(information => information.category.includes(keyword) || information.name.includes(keyword) || information.name_en.toLowerCase().includes(keyword.toLowerCase()))
+  const information = restaurantList.results.filter(info => (info.category + info.name + info.name_en.split(' ').join('')).toLowerCase().includes(keyword))
 
   // 搜尋時會重複多次 search 路由，所以每次進入都要重新管理 findingStatus 的狀態
   if (information.length > 0) {
-    findingStatus = true
-    res.render('index', { restaurants: information, findingStatus: findingStatus, keyword: keyword})
+    res.render('index', { restaurants: information, findingStatus: true, keyword: originKeyword})
   } else {
-    findingStatus = false
-    res.render('index', { restaurants: restaurantList.results, findingStatus: findingStatus, keyword: keyword })
+    res.render('index', { restaurants: restaurantList.results, findingStatus: false, keyword: originKeyword})
   }
 })
 app.listen(port, () => {
