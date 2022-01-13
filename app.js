b5bbdb6698b213f00a2e7454d3b8a0b8c9de75d6
Marcus Liu <me@mail.lmarcus.onl>
2022-01-14 00:27:20 +0800

    feat: 改寫路由 /restaurants/:restaurantId 完成

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.use(express.static('public'))
 // 餐廳清單
 const restaurantList = require('./restaurant.json')
 
+// import body parser
+app.use(express.urlencoded({ extended: true }))
+
 // import mongodb
 const mongoose = require('mongoose')
 
@@ -41,10 +44,9 @@ app.get('/', (req, res) => {
 
 // 餐廳資料
 app.get('/restaurants/:restaurantId', (req, res) => {
-  const restaurant = restaurantList.results.find(function findRestaurantName (restaurant) {
-    return restaurant.id.toString() === req.params.restaurantId
-  })
-  res.render('restaurantinfo', { restaurant: restaurant })
+  return Restaurant.findOne({ id: req.params.restaurantId }).lean()
+    .then(restaurant => res.render('restaurantinfo', { restaurant }))
+    .catch(error => console.error(error))
 })
 
 // 搜尋
@@ -56,7 +58,6 @@ app.get('/search', (req, res) => {
   // 只要關鍵字符合其中一個，就返回內容到陣列
   const information = restaurantList.results.filter(info => (info.category + info.name + info.name_en.split(' ').join('')).toLowerCase().includes(keyword))
 
-  // 搜尋時會重複多次 search 路由，所以每次進入都要重新管理 findingStatus 的狀態
   if (information.length > 0) {
     res.render('index', { restaurants: information, findingStatus: true, keyword: originKeyword})
   } else {
