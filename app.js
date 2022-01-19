ed7b24623014a10b6a9cbf2a0d9344f24e07a13c
Marcus Liu <me@mail.lmarcus.onl>
2022-01-20 04:54:03 +0800

    release: ac-restaurant v1.2.0

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const port = 3000
 
 // import and setting handlebars
 const exphbs = require('express-handlebars')
+const hbshelpers = require('handlebars-helpers')
+const multihelpers = hbshelpers()
 app.set('view engine', 'handlebars')
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs({
+  helpers: multihelpers, 
+  defaultLayout: 'main'
+ }))
 // import local static files
 // css 和 js
 app.use(express.static('public'))
@@ -47,7 +52,7 @@ app.get('/', (req, res) => {
 
 // 餐廳資料
 app.get('/restaurants/:restaurantId', (req, res) => {
-  return Restaurant.findOne({ id: req.params.restaurantId }).lean()
+  return Restaurant.findOne({ _id: req.params.restaurantId }).lean()
     .then(restaurant => res.render('restaurantinfo', { restaurant }))
     .catch(error => console.error(error))
 })
@@ -71,6 +76,61 @@ app.get('/search', (req, res) => {
     })
     .catch(error => console.error(error))
 })
+
+// 新增餐廳相關
+// 渲染新增頁面
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+// 新增表單 post 相關邏輯 
+app.post('/new', (req, res) => {
+  return Restaurant.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+})
+
+// 編輯餐廳相關
+// 渲染編輯頁面
+app.get('/restaurants/edit/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => console.error(error))
+})
+
+// 實作編輯功能
+// 暫時找不到原本 json 檔裡的數字 id 怎麽轉，先放著不修(這應該是原始資料設計的問題)，之後重構再弄
+app.post('/restaurants/edit/:id', (req, res) => {
+  const id = req.params.id
+  const {productId, name, name_en, category, image, location, phone, google_map, rating, description} = req.body
+  Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.error(error))
+})
+
+// 實作刪除功能
+app.get('/restaurants/delete/:id', (req, res) => {
+  const id = req.params.id
+  Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`ac-restaurantList is running on http://localhost:${port}`)
 })
