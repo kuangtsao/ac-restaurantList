4273714db807f06aabc7942a98ecc57d0906311d
Marcus Liu <me@mail.lmarcus.onl>
2022-01-22 00:38:01 +0800

    refactor: mongoose 連線 config 化

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -3,13 +3,8 @@ const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
 const restaurantList = require('../../restaurant.json').results
 
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true,useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+// mongoose 連線設定
+require('../../config/mongoose')
 
 db.once('open', () => {
   console.log('mongodb connected!')
