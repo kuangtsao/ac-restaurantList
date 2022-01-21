df3a02bc223f1749df5ca48369243738921c19c3
Marcus Liu <me@mail.lmarcus.onl>
2022-01-21 12:23:02 +0800

    refactor: 重構餵入種子資料的方式

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,7 +1,7 @@
 // 載入 mongoose 與 restaurant model
 const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
-const restaurantList = require('../../restaurant.json')
+const restaurantList = require('../../restaurant.json').results
 
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true,useUnifiedTopology: true })
 
@@ -15,20 +15,10 @@ db.once('open', () => {
   console.log('mongodb connected!')
   console.log('loading seeds from restaurant.js')
 
-  for(let item of restaurantList.results) {
-    Restaurant.create({
-      id: item.id,
-      name: item.name,
-      name_en: item.name_en,
-      category: item.category,
-      image: item.image,
-      location: item.location,
-      phone: item.phone,
-      google_map: item.google_map,
-      rating: item.rating,
-      description: item.description
+  Restaurant.create(restaurantList)
+    .then(() => {
+      console.log('all seeds is loaded.')
     })
-  }
-  
-  console.log('all seeds is loaded.')
+    .catch(err => console.log(err))
+    .finally(() => process.exit())
 })
