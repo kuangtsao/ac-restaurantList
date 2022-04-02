61ef4078101c90fdaacfeb7f79f376c1d49a70b5
Marcus Liu <me@mail.lmarcus.onl>
2022-04-03 00:19:19 +0800

    style: 整理 index.js 排版

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const users = require('./modules/users')
-router.use('/users', users)
-
 const home = require('./modules/home')
-router.use('/', home)
-
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
+
+router.use('/users', users)
 router.use('/restaurants', restaurants)
+router.use('/', home)
 
 module.exports = router
