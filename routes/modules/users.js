0091f4e2b3fd2cf66b34c05f4d9c2a4defd5cbc6
Marcus Liu <me@mail.lmarcus.onl>
2022-04-06 12:12:11 +0800

    fix: 符合「email 與密碼是必須欄位，但名字不是」的規格

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -15,8 +15,8 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
   const errors = []
-  if (!name || !email || !password || !confirmPassword) {
-    errors.push({ message: '所有欄位都是必填' })
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: 'email, 密碼, 確認密碼必須填入！' })
   }
   if (password !== confirmPassword) {
     errors.push({ message: '密碼與確認密碼不相符！' })
